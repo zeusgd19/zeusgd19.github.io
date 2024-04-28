7b140a2ebeaaa3e2e3b80a8eb9523cd3985eac59
zeusgd19 <dariusgd19@gmail.com>
2024-04-28 18:55:41 +0200

    Coreccion Calculadora

--- Calculadora/JavaScript/index.js
+++ Calculadora/JavaScript/index.js
@@ -7,7 +7,7 @@ for(let i = 0; i < $botones.length; i++){
     $botones[i].addEventListener("click", controlar)
 }
 
-
+let pressed = false
 function controlar(e){
     switch(e.target.innerText){
         case "C":
@@ -15,12 +15,17 @@ function controlar(e){
             break;
         case "=":
             $textArea.value = eval($textArea.value)
+            pressed = true
             break;
         default:
-            if($textArea.value == "0"){
+            if($textArea.value == "0" || pressed && e.target.innerText != "+"
+                && e.target.innerText != "-"
+                && e.target.innerText != "*"
+                && e.target.innerText != "/"){
                 $textArea.value = ""
             }
             $textArea.value += e.target.innerText
+            pressed = false
             break;
     }
 }
