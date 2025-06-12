975a2336cac8771cc5d5fe0b15d5f9688caf2f2e
zeusgd19 <dariusgd19@gmail.com>
2025-06-12 17:03:44 +0200

    Cambios dist

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      react(),
-      tailwindcss()
-  ],
+    base: "/zeusgd19.github.io/",
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-})
+});
